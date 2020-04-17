899b56103aa6b8a0883e94f05563dc248c51e3cf
DilshodN <dilshod8471@gmail.com>
2020-04-17 14:18:48 +0500

    edit puzzle.py

--- puzzle.py
+++ puzzle.py
@@ -8,11 +8,6 @@ class Puzzle:
         self.board = board
         self.width = len(board)
         self.goal = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
-        self.goal_idx = {
-            1: (0, 0), 2: (0, 1), 3: (0, 2),
-            4: (1, 0), 5: (1, 1), 6: (1, 2),
-            7: (2, 0), 8: (2, 1), 0: (2, 2)
-        }
 
     def __repr__(self):
         table = bt.BeautifulTable()
@@ -20,27 +15,31 @@ class Puzzle:
             table.append_row(self.board[k])
         return str(table)
 
+    def get_idx(self, board, value)->(int, int):
+        for i in range(self.width):
+            for j in range(self.width):
+                if board[i][j] == value:
+                    return i, j
+
     def solved(self) -> bool:
         return self.board == self.goal
 
     def hamming(self) -> int:
         hamming = 0
-        for i in range(3):
-            for j in range(3):
+        for i in range(self.width):
+            for j in range(self.width):
                 if self.board[i][j] != self.goal[i][j] and self.board[i][j] != 0:
                     hamming += 1
         return hamming
 
     def manhattan(self) -> int:
         manhattan = 0
-        for i in range(3):
-            for j in range(3):
+        for i in range(self.width):
+            for j in range(self.width):
                 my_num = self.board[i][j]
-                x, y = self.goal_idx[my_num]
+                x, y = self.get_idx(self.goal, my_num)
                 manhattan += abs((x + y) - (i + j))
         return manhattan
-
-
 
 
 def main():
